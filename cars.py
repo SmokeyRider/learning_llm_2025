ac9e561bb22bd493c420e789143670e9b6d937b6
Odin Anderson (from Dev Box) <odina@microsoft.com>
2025-02-19 12:35:33 -0800

    Implement main function to demonstrate vehicle handling and display. and added dunder func str

--- cars.py
+++ cars.py
@@ -1,54 +1,113 @@
+"""
+Odin's Object Oriented Programming learning playground in Python.
+
+This module defines a Vehicle class and a VehicleType enumeration to represent different types of vehicles.
+It also demonstrates creating instances of the Vehicle class, sorting them, and filtering them based on their type.
+"""
+
 from enum import Enum
 
 class VehicleType(Enum):
+    """
+    Enumeration for different types of vehicles.
+    """
     CAR = "Car"
     MOTORCYCLE = "Motorcycle"
     BIKE = "Bike"
 
 class Vehicle:
+    """
+    A class to represent a vehicle.
+
+    Attributes:
+    make (str): The make of the vehicle.
+    model (str): The model of the vehicle.
+    year (int): The year the vehicle was manufactured.
+    driver (str): The name of the driver.
+    type (VehicleType): The type of the vehicle (default is VehicleType.CAR).
+    """
+
     def __init__(self, make: str, model: str, year: int, driver: str, type: VehicleType = VehicleType.CAR):
+        """
+        Initialize the Vehicle instance.
+
+        Parameters:
+        make (str): The make of the vehicle.
+        model (str): The model of the vehicle.
+        year (int): The year the vehicle was manufactured.
+        driver (str): The name of the driver.
+        type (VehicleType): The type of the vehicle (default is VehicleType.CAR).
+        """
         self.make: str = make
         self.model: str = model
         self.year: int = year
         self.driver: str = driver
         self.type: VehicleType = type
 
+    def __str__(self):
+        """
+        Return the string representation of the vehicle.
+        """
+        return f"{self.year} {self.type.value} {self.make} {self.model} - Driver: {self.driver}"
+    
     def display(self):
+        """
+        Display the details of the vehicle.
+        """
         print(f"Year: {self.year}, Type: {self.type.value}, Make: {self.make}, Model: {self.model}, Driver: {self.driver}")
 
-vehicles = []
-vehicles.append(Vehicle("BMW", "328i", 2015, 'Ruby'))
-vehicles.append(Vehicle("Ford", "Explorer", 2015, "Evie"))
-vehicles.append(Vehicle("Tesla", "Model S", 2021, "Odin"))
-vehicles.append(Vehicle("Harley", "FLSTC", 2004, "Odin", type=VehicleType.MOTORCYCLE))
-vehicles.append(Vehicle("Aventon", "Ramblas", 2024, "Odin", type=VehicleType.BIKE))
-vehicles.append(Vehicle("Aventon", "Pace 500", 2024, "Evie", type=VehicleType.BIKE))
-vehicles.append(Vehicle("Polygon", "Siskiu T8", 2024, "Odin", type=VehicleType.BIKE))
-vehicles.append(Vehicle("GT", "Avalance 2.0", 2003, "Odin", type=VehicleType.BIKE))
+def main():
+    """
+    The main function to demonstrate the Vehicle class and VehicleType enumeration.
+    """
+    # List to store vehicle instances
+    vehicles = []
 
-vehicles.sort(key=lambda x: x.year)
+    # Adding vehicle instances to the list
+    vehicles.append(Vehicle("BMW", "328i", 2015, 'Ruby'))
+    vehicles.append(Vehicle("Ford", "Explorer", 2015, "Evie"))
+    vehicles.append(Vehicle("Tesla", "Model S", 2021, "Odin"))
+    vehicles.append(Vehicle("Harley", "FLSTC", 2004, "Odin", type=VehicleType.MOTORCYCLE))
+    vehicles.append(Vehicle("Aventon", "Ramblas", 2024, "Odin", type=VehicleType.BIKE))
+    vehicles.append(Vehicle("Aventon", "Pace 500.3", 2024, "Evie", type=VehicleType.BIKE))
+    vehicles.append(Vehicle("Polygon", "Siskiu T8", 2024, "Odin", type=VehicleType.BIKE))
+    vehicles.append(Vehicle("GT", "Avalance 2.0", 2003, "Odin", type=VehicleType.BIKE))
 
-print("All Vehicles:")
-for vehicle in vehicles:
-    vehicle.display()
+    # Sorting vehicles by year
+    vehicles.sort(key=lambda x: x.year)
 
-# Select all bikes using list comprehension (case-insensitive)
-bikes = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.BIKE]
+    # Display all vehicles
+    print("All Vehicles:")
+    for vehicle in vehicles:
+        vehicle.display()
 
-print("\nBikes:")
-for bike in bikes:
-    bike.display()
+    # Select all bikes using list comprehension
+    bikes = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.BIKE]
 
-# Select all cars using list comprehension
-cars = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.CAR]
+    # Display all bikes
+    print("\nBikes:")
+    for bike in bikes:
+        bike.display()
 
-print("\nCars:")
-for car in cars:
-    car.display()
+    # Select all cars using list comprehension
+    cars = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.CAR]
 
-# Select all motorcycles using list comprehension
-motorcycles = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.MOTORCYCLE]
+    # Display all cars
+    print("\nCars:")
+    for car in cars:
+        car.display()
 
-print("\nMotorcycles:")
-for motorcycle in motorcycles:
-    motorcycle.display()
+    # Select all motorcycles using list comprehension
+    motorcycles = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.MOTORCYCLE]
+
+    # Display all motorcycles
+    print("\nMotorcycles:")
+    for motorcycle in motorcycles:
+        motorcycle.display()
+
+    # display the last vehicle
+    print("\nLast Vehicle:")
+    print(vehicles[-1])
+
+if __name__ == "__main__":
+    main()
