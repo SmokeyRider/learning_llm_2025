14a2635d8d439fa0ed4f6211a2440e62024e7f9b
Odin Anderson (from Dev Box) <odina@microsoft.com>
2025-02-19 12:16:58 -0800

    Refactor car handling to a more flexible Vehicle class; add support for multiple vehicle types and enhanced display functionality

--- cars.py
+++ cars.py
@@ -1,18 +1,54 @@
-class cars:
-  def __init__(self, make, model, year):
-    self.make = make
-    self.model = model
-    self.year = year
+from enum import Enum
 
-  def __str__(self):
-    return f"{self.make} {self.model} {self.year}"
-  
-  def display(self):
-    print(self.make, self.model, self.year)
+class VehicleType(Enum):
+    CAR = "Car"
+    MOTORCYCLE = "Motorcycle"
+    BIKE = "Bike"
 
-bmer = cars("BMW", "328i", 2015)
-explorer = cars("Ford", "Explorer", 2015)
-tesla = cars("Tesla", "Model S", 2021)
+class Vehicle:
+    def __init__(self, make: str, model: str, year: int, driver: str, type: VehicleType = VehicleType.CAR):
+        self.make: str = make
+        self.model: str = model
+        self.year: int = year
+        self.driver: str = driver
+        self.type: VehicleType = type
 
-#bmer.display()
-print('My cars:', bmer, explorer, tesla, sep="\n\t")
+    def display(self):
+        print(f"Year: {self.year}, Type: {self.type.value}, Make: {self.make}, Model: {self.model}, Driver: {self.driver}")
+
+vehicles = []
+vehicles.append(Vehicle("BMW", "328i", 2015, 'Ruby'))
+vehicles.append(Vehicle("Ford", "Explorer", 2015, "Evie"))
+vehicles.append(Vehicle("Tesla", "Model S", 2021, "Odin"))
+vehicles.append(Vehicle("Harley", "FLSTC", 2004, "Odin", type=VehicleType.MOTORCYCLE))
+vehicles.append(Vehicle("Aventon", "Ramblas", 2024, "Odin", type=VehicleType.BIKE))
+vehicles.append(Vehicle("Aventon", "Pace 500", 2024, "Evie", type=VehicleType.BIKE))
+vehicles.append(Vehicle("Polygon", "Siskiu T8", 2024, "Odin", type=VehicleType.BIKE))
+vehicles.append(Vehicle("GT", "Avalance 2.0", 2003, "Odin", type=VehicleType.BIKE))
+
+vehicles.sort(key=lambda x: x.year)
+
+print("All Vehicles:")
+for vehicle in vehicles:
+    vehicle.display()
+
+# Select all bikes using list comprehension (case-insensitive)
+bikes = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.BIKE]
+
+print("\nBikes:")
+for bike in bikes:
+    bike.display()
+
+# Select all cars using list comprehension
+cars = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.CAR]
+
+print("\nCars:")
+for car in cars:
+    car.display()
+
+# Select all motorcycles using list comprehension
+motorcycles = [vehicle for vehicle in vehicles if vehicle.type == VehicleType.MOTORCYCLE]
+
+print("\nMotorcycles:")
+for motorcycle in motorcycles:
+    motorcycle.display()
